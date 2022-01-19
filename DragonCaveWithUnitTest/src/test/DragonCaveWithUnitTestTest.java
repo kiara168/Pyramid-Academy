a4cfd0d1727d4682270db3640e88e0e833abb898
Kiara <kiaraharris168@gmail.com>
2022-01-19 12:59:01 -0500

    Work on DragonCaveWithUnitTest
    split code into methods and started writing unit tests

--- DragonCaveWithUnitTest/src/test/DragonCaveWithUnitTestTest.java
+++ DragonCaveWithUnitTest/src/test/DragonCaveWithUnitTestTest.java
@@ -11,11 +11,23 @@ class DragonCaveWithUnitTestTest {
     }
 
     @Test
-    void main(String[] args) {
-        int a = 1;
-        System.out.print(DragonCaveWithUnitTest.main(String[] args));
-        assertEquals("",DragonCaveWithUnitTest.main(String[] args),"Test failed");
+    String Intro(){
+        System.out.print(Intro());
+        assertEquals("You are in a land full of Dragons. In front of you, \nyou see two caves. In one cave, the dragon is friendly \nand will share his treasure with you. The other dragon \nis greedy and hungry and will eat you on sight. \nWhich cave will you go into? (1 or 2)", Intro(), "test failed");
+        return Intro();
     }
+
+    @Test
+    void WhichDragon(int x){
+        System.out.print(DragonCaveWithUnitTest.WhichDragon(1));
+        System.out.print(WhichDragon (2));
+    }
+
+    //@Test
+   // void main(String[] args) {
+        //System.out.print(main(String[] args));
+        //assertEquals("",main(String[] args),"Test failed");
+   // }
 
     @AfterEach
     void tearDown() {
