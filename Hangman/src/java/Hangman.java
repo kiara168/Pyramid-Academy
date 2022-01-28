5d02325f5c6797fe2886bbc50df3d3ec48e65f03
Kiara <kiaraharris168@gmail.com>
2022-01-28 13:53:03 -0500

    Created Hangman Project

---
+++ Hangman/src/java/Hangman.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Hangman {
+    //computer thinks of a word and draws a blank line for each letter of the word
+    //user inputs guesses for letters in the word.
+    //main:
+    //make hangman drawing
+    //get random word from computer
+    //create blank spaces for that word
+    //call method 1
+    // as many guess as length of word
+    //allow for play again
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+    //method 1
+    //get user guess, if guess in word, put it into the right spot in blanks
+    //if guess not in word, add to the hangman /call method 2
+    //if guess already guessed choose another guess
+    public static String getGuesses(int x){
+
+        return String;
+    }
+
+    //method 2
+    // top of hangman is an O, rest is vertical dashes
+    public static String drawHangman(String){
+
+        return String;
+    }
+}
