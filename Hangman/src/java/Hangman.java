bd5a023b37343d9c39f322ae1aec4800ae9b292b
Kiara <kiaraharris168@gmail.com>
2022-01-31 16:08:24 -0500

    Worked on Hangman project

--- Hangman/src/java/Hangman.java
+++ Hangman/src/java/Hangman.java
@@ -34,140 +34,135 @@ public class Hangman {
 //    }
 
     // We are going to create a Hangman Game with Java keywords :)
-    public class HangmanGame {
 
-        // Java Keywords
-        public static final String[] WORDS = {
-                "ABSTRACT", "ASSERT", "BOOLEAN", "BREAK", "BYTE",
-                "CASE", "CATCH", "CHAR", "CLASS", "CONST",
-                "CONTINUE", "DEFAULT", "DOUBLE", "DO", "ELSE",
-                "ENUM", "EXTENDS", "FALSE", "FINAL", "FINALLY",
-                "FLOAT", "FOR", "GOTO", "IF", "IMPLEMENTS",
-                "IMPORT", "INSTANCEOF", "INT", "INTERFACE",
-                "LONG", "NATIVE", "NEW", "NULL", "PACKAGE",
-                "PRIVATE", "PROTECTED", "PUBLIC", "RETURN",
-                "SHORT", "STATIC", "STRICTFP", "SUPER", "SWITCH",
-                "SYNCHRONIZED", "THIS", "THROW", "THROWS",
-                "TRANSIENT", "TRUE", "TRY", "VOID", "VOLATILE", "WHILE"
-        };
+//    public void playAgain(String ans){
+//        while(ans.equalsIgnoreCase("yes")){
+//            play();
+//        }
+//    }
 
-        public static final Random RANDOM = new Random();
-        // Max errors before user lose
-        public static final int MAX_ERRORS = 8;
-        // Word to find
-        private String wordToFind;
-        // Word found stored in a char array to show progression of user
-        private char[] wordFound;
-        private int nbErrors;
-        // letters already entered by user
-        private ArrayList< String > letters = new ArrayList < > ();
+    private static String[] words = {"terminator", "banana", "computer", "cow", "rain", "water" };
+    private static String word = words[(int) (Math.random() * words.length)];
+    private static String asterisk = new String(new char[word.length()]).replace("\0", "*");
+    private static int count = 0;
 
-        // Method returning randomly next word to find
-        private String nextWordToFind() {
-            return WORDS[RANDOM.nextInt(WORDS.length)];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (count < 7 && asterisk.contains("*")) {
+            System.out.println("Guess any letter in the word");
+            System.out.println(asterisk);
+            String guess = sc.next();
+            hang(guess);
         }
+        sc.close();
+    }
 
-        // Method for starting a new game
-        public void newGame() {
-            nbErrors = 0;
-            letters.clear();
-            wordToFind = nextWordToFind();
-
-            // word found initialization
-            wordFound = new char[wordToFind.length()];
-
-            for (int i = 0; i < wordFound.length; i++) {
-                wordFound[i] = '_';
+    public static void hang(String guess) {
+        String newasterisk = "";
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == guess.charAt(0)) {
+                newasterisk += guess.charAt(0);
+            } else if (asterisk.charAt(i) != '*') {
+                newasterisk += word.charAt(i);
+            } else {
+                newasterisk += "*";
             }
         }
 
-        // Method returning trus if word is found by user
-        public boolean wordFound() {
-            return wordToFind.contentEquals(new String(wordFound));
+        if (asterisk.equals(newasterisk)) {
+            count++;
+            hangmanImage();
+        } else {
+            asterisk = newasterisk;
         }
-
-        // Method updating the word found after user entered a character
-        private void enter(String c) {
-            // we update only if c has not already been entered
-            if (!letters.contains(c)) {
-                // we check if word to find contains c
-                if (wordToFind.contains(c)) {
-                    // if so, we replace _ by the character c
-                    int index = wordToFind.indexOf(c);
-
-                    while (index >= 0) {
-                        wordFound[index] = c.charAt(0);
-                        index = wordToFind.indexOf(c, index + 1);
-                    }
-                } else {
-                    // c not in the word => error
-                    nbErrors++;
-                }
-
-                // c is now a letter entered
-                letters.add(c);
-            }
+        if (asterisk.equals(word)) {
+            System.out.println("Correct! You win! The word was " + word);
         }
+    }
 
-        // Method returning the state of the word found by the user until by now
-        private String wordFoundContent() {
-            StringBuilder builder = new StringBuilder();
-
-            for (int i = 0; i < wordFound.length; i++) {
-                builder.append(wordFound[i]);
-
-                if (i < wordFound.length - 1) {
-                    builder.append(" ");
-                }
-            }
-
-            return builder.toString();
+    public static void hangmanImage() {
+        if (count == 1) {
+            System.out.println("Wrong guess, try again");
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println("___|___");
+            System.out.println();
         }
-
-        // Play method for our Hangman Game
-        public void play() {
-            try (Scanner input = new Scanner(System.in)) {
-                // we play while nbErrors is lower than max errors or user has found the word
-                while (nbErrors < MAX_ERRORS) {
-                    System.out.println("\nGuess a letter : ");
-                    // get next input from user
-                    String str = input.next();
-
-                    // we keep just first letter
-                    if (str.length() > 1) {
-                        str = str.substring(0, 1);
-                    }
-
-                    // update word found
-                    enter(str);
-
-                    // display current state
-                    System.out.println("\n" + wordFoundContent());
-
-                    // check if word is found
-                    if (wordFound()) {
-                        System.out.println("\nYou win!");
-                        break;
-                    } //else {
-                        // we display nb tries remaining for the user
-                        //System.out.println("\n=> Nb tries remaining : " + (MAX_ERRORS - nbErrors));
-                   // }
-                }
-
-                if (nbErrors == MAX_ERRORS) {
-                    // user losed
-                    System.out.println("\nYou lose!");
-                    System.out.println("=> Word to find was : " + wordToFind);
-                }
-            }
+        if (count == 2) {
+            System.out.println("Wrong guess, try again");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
         }
-
-        public static void main(String[] args) {
-            System.out.println("HANGMAN");
-            HangmanGame hangmanGame = new HangmanGame();
-            hangmanGame.newGame();
-            hangmanGame.play();
+        if (count == 3) {
+            System.out.println("Wrong guess, try again");
+            System.out.println("   ____________");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   | ");
+            System.out.println("___|___");
         }
-
+        if (count == 4) {
+            System.out.println("Wrong guess, try again");
+            System.out.println("   ____________");
+            System.out.println("   |          _|_");
+            System.out.println("   |         /   \\");
+            System.out.println("   |        |     |");
+            System.out.println("   |         \\_ _/");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
+        }
+        if (count == 5) {
+            System.out.println("Wrong guess, try again");
+            System.out.println("   ____________");
+            System.out.println("   |          _|_");
+            System.out.println("   |         /   \\");
+            System.out.println("   |        |     |");
+            System.out.println("   |         \\_ _/");
+            System.out.println("   |           |");
+            System.out.println("   |           |");
+            System.out.println("   |");
+            System.out.println("___|___");
+        }
+        if (count == 6) {
+            System.out.println("Wrong guess, try again");
+            System.out.println("   ____________");
+            System.out.println("   |          _|_");
+            System.out.println("   |         /   \\");
+            System.out.println("   |        |     |");
+            System.out.println("   |         \\_ _/");
+            System.out.println("   |           |");
+            System.out.println("   |           |");
+            System.out.println("   |          / \\ ");
+            System.out.println("___|___      /   \\");
+        }
+        if (count == 7) {
+            System.out.println("GAME OVER!");
+            System.out.println("   ____________");
+            System.out.println("   |          _|_");
+            System.out.println("   |         /   \\");
+            System.out.println("   |        |     |");
+            System.out.println("   |         \\_ _/");
+            System.out.println("   |          _|_");
+            System.out.println("   |         / | \\");
+            System.out.println("   |          / \\ ");
+            System.out.println("___|___      /   \\");
+            System.out.println("GAME OVER! The word was " + word);
+        }
     }
 }
+
